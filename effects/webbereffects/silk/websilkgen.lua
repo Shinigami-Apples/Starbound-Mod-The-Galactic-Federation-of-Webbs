7e6831e7858f2313dd4c38031809763b9614076c
Vetpetmon <Vetpetmon@hotmail.com>
2018-04-01 11:31:11 -0400

    Typing "//" in a lua file in Atom crashes it
    lmao what

--- effects/webbereffects/silk/websilkgen.lua
+++ effects/webbereffects/silk/websilkgen.lua
@@ -1,19 +1,26 @@
+--Credit goes to C0bra5 on the Starbound discord for fixing this!
+--Go check out his awesome mod @ https://steamcommunity.com/sharedfiles/filedetails/?id=1348805475
+--vvv Prevents that nasty equip exploit
 function init()
-  script.setUpdateDelta(config.getParameter("refresh"))
+    script.setUpdateDelta(config.getParameter("refresh"))
+    self.started = false;
+    self.item = config.getParameter("item")
+    self.maxAmount = config.getParameter("maxAmount")
+    self.configAmount = config.getParameter("amount")
 end
 
 function update(dt)
-    item = config.getParameter("item")
-    count = world.entityHasCountOfItem(entity.id(), item)
-    maxAmount = config.getParameter("maxAmount")
-    configAmount = config.getParameter("amount")
-    maxDrop = math.min(configAmount, 1000)
-    for _, entityID in pairs (world.itemDropQuery(entity.position(), 5)) do
-        if world.entityName(entityID) == item then
-            return
+    if self.started then
+        local count = world.entityHasCountOfItem(entity.id(), self.item)
+        local maxDrop = math.min(self.configAmount, 1000)
+        for _, entityID in pairs (world.itemDropQuery(entity.position(), 5)) do
+            if world.entityName(entityID) == self.item then
+                return
+            end
+        end
+        if count and count < self.maxAmount then
+            world.spawnItem(self.item, entity.position(), math.min(self.configAmount, self.maxAmount - count), {price = 0})
         end
     end
-    if count and count < maxAmount then
-        world.spawnItem(item, entity.position(), math.min(configAmount, maxAmount - count), {price = 0})
-    end
+    self.started = true;
 end
