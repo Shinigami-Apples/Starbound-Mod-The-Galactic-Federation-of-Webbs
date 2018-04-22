7d3fbc8fe88ed3b4359d12a8f8cd45723274e1ec
Vetpetmon <Vetpetmon@hotmail.com>
2018-04-22 12:15:48 -0400

    feex

--- stats/effects/combat/stunlocked/combatwebstunned1.lua
+++ stats/effects/combat/stunlocked/combatwebstunned1.lua
@@ -1,8 +1,9 @@
+
 function init()
   effect.setParentDirectives("fade=7733AA=0.25")
   effect.addStatModifierGroup({
     {stat = "jumpModifier", amount = -0.3},
-    {stat = "maxEnergy", amount = config.getParameter("energyAmount", 0.4)} --player may be unable to use ranged, but can still use basic melee.
+    {stat = "maxEnergy", amount = config.getParameter("energyAmount", -700.0)} --player may be unable to use ranged, but can still use basic melee.
   })
 end
 
