5474ccd2284c2cae594050ad2225cf867a874e03
Vetpetmon <Vetpetmon@hotmail.com>
2018-04-22 10:19:50 -0400

    Well shit. Let's NOT piss off the space cops.
    goodbye, lawbreakers.

---
+++ stats/effects/combat/combatwebstuned1.lua
@@ -0,0 +1,19 @@
+function init()
+  effect.setParentDirectives("fade=7733AA=0.25")
+  effect.addStatModifierGroup({
+    {stat = "jumpModifier", amount = -0.3},
+    {stat = "maxEnergy", amount = config.getParameter("energyAmount", 0.4)} --player may be unable to use ranged, but can still use basic melee.
+  })
+end
+
+function update(dt)
+  mcontroller.controlModifiers({
+      groundMovementModifier = 0.1,
+      speedModifier = 0.1,
+      airJumpModifier = 0.1 --hecc you, you're unable to move. period.
+    })
+end
+
+function uninit()
+
+end
