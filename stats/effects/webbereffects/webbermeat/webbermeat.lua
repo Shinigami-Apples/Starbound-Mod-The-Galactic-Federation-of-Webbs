152ef34403b037aa302bf8ea7e213d581736a484
Vetpetmon <Vetpetmon@hotmail.com>
2018-05-13 17:24:29 -0400

    tweaks

--- stats/effects/webbereffects/webbermeat/webbermeat.lua
+++ stats/effects/webbereffects/webbermeat/webbermeat.lua
@@ -7,7 +7,7 @@ function init()
 
   self.species=world.entitySpecies(entity.id())
   if self.species ~= "webber" then
-    self.effectHandler=effect.addStatModifierGroup({{stat = "foodDelta", baseMultiplier = 4.0}})
+    self.effectHandler=effect.addStatModifierGroup({{stat = "foodDelta", baseMultiplier = 20.0}})
     world.sendEntityMessage(entity.id(), "queueRadioMessage", "webbermeatillness",1)
   elseif self.species == "webber" then
     self.healingRate = config.getParameter("healAmount", 20) / effect.duration()
@@ -15,30 +15,16 @@ function init()
 end
 
 function update(dt)
-  if self.species ~= "webber" then
-    sicken()
-  elseif self.species == "webber" then
+  if self.species == "webber" then
     boost()
   end
 end
 
-function sicken()
-  self.tickTimer = self.tickTimer - dt
-  if self.tickTimer <= 0 then
-    self.tickTimer = self.tickTime
-    status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 5,
-        damageSourceKind = "poison",
-        sourceEntityId = entity.id()
-      })
-  end
-end
 
 function boost()
         status.modifyResource("health", self.healingRate * dt)
 end
 
 function uninit()
-  status.clearPersistentEffects("webberimmunity")
+
 end
