8946b535788a0de868c96bc22d7415a5c96a5d4b
Vetpetmon <Vetpetmon@hotmail.com>
2018-06-18 16:21:18 -0400

    wastelander set and fixes

--- stats/effects/webbereffects/webbermeat/webbermeat.lua
+++ stats/effects/webbereffects/webbermeat/webbermeat.lua
@@ -1,7 +1,7 @@
 function init()
     self.species=world.entitySpecies(entity.id())
 
-  if self.species ~= "webber" or "glitch" or "droden" or "trink" or "radien" or "novakid" or "elunite" or "indix" then
+  if self.species ~= "webber" then
     self.effectHandler=effect.addStatModifierGroup({{stat = "foodDelta", baseMultiplier = 20.0}})
     world.sendEntityMessage(entity.id(), "queueRadioMessage", "webbermeatillness",1)
   else
