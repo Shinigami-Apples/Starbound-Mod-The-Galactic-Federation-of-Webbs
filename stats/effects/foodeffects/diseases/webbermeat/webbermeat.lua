d5ffb1eaf67503bd1a74b1b4074d0219a60a3c65
Vetpetmon-Labs <vetpetmon@hotmail.com>
2018-07-19 14:18:09 -0400

    Fishing for webbers using canned food

--- stats/effects/foodeffects/diseases/webbermeat/webbermeat.lua
+++ stats/effects/foodeffects/diseases/webbermeat/webbermeat.lua
@@ -4,6 +4,7 @@ function init()
   if self.species ~= "webber" then
     self.effectHandler=effect.addStatModifierGroup({{stat = "foodDelta", baseMultiplier = 20.0}})
     world.sendEntityMessage(entity.id(), "queueRadioMessage", "webbermeatillness",1)
+    status.removeEphemeralEffect("wellfed",math.huge)
   else
     status.removeEphemeralEffect("webbermeat",math.huge)
   end
